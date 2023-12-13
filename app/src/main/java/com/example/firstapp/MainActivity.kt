bc3f262b9256d17b6f372c5bc8d563aac99f5f59
Akarumai <barabashka040504@gmail.com>
2023-12-13 04:00:27 +0300

    ready quiz

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,121 +1,122 @@
 package com.example.firstapp
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
-import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.ViewModelProvider
-import com.example.firstapp.models.Question
 import com.example.firstapp.viewmodels.QuizViewModel
 
 private const val TAG = "MainActivity"
 private const val KEY_INDEX = "index"
 
-class MainActivity : AppCompatActivity() {
+open class MainActivity : AppCompatActivity() {
+
 
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: ImageButton
-    private lateinit var backButton: ImageButton
+    private lateinit var sumbitButton: Button
+    private lateinit var checkAnwser: Button
     private lateinit var questionTextView: TextView
+    private lateinit var returnButton: Button
 
     private val quizViewModel: QuizViewModel by lazy {
         ViewModelProvider(this).get(QuizViewModel::class.java)
     }
-
-
     var countSuccessQuestions = 0;
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
+
         val currentIndex = savedInstanceState?.getInt(KEY_INDEX, 0) ?: 0
         quizViewModel.currentIndex = currentIndex
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
-        nextButton = findViewById(R.id.next_button)
-        backButton = findViewById(R.id.back_button)
+        sumbitButton = findViewById(R.id.sucess_button)
+        checkAnwser = findViewById(R.id.check_answer)
+        returnButton = findViewById(R.id.return_button)
         questionTextView = findViewById(R.id.question_text_view)
 
-
+        sumbitButton.isEnabled = false
         trueButton.setOnClickListener { view: View ->
             checkAnswer(true)
-
+            trueButton.isEnabled = false
+            falseButton.isEnabled = false
+            sumbitButton.isEnabled = true
         }
 
         falseButton.setOnClickListener { view: View ->
             checkAnswer(false)
+            trueButton.isEnabled = false
+            falseButton.isEnabled = false
+            sumbitButton.isEnabled = true
         }
 
-        nextButton.setOnClickListener {
-            quizViewModel.moveToNext()
+        sumbitButton.setOnClickListener {
+            trueButton.isEnabled = true
+            falseButton.isEnabled = true
+            sumbitButton.isEnabled = false
+            if (quizViewModel.currentIndex == quizViewModel.questionBank.size - 1) {
+                trueButton.isEnabled = false
+                falseButton.isEnabled = false
+                falseButton.isEnabled = false
+                returnButton.isEnabled = true
+            } else {
+                quizViewModel.moveToNext()
+            }
             updateQuestion()
         }
 
-        backButton.setOnClickListener {
-            quizViewModel.moveToBack()
+        returnButton.isEnabled = false
+        returnButton.setOnClickListener {
+            countSuccessQuestions = 0
+            quizViewModel.currentIndex = 0
             updateQuestion()
+            Toast.makeText(this, "Квиз запущен заново", Toast.LENGTH_SHORT).show()
+            trueButton.isEnabled = true
+            falseButton.isEnabled = true
+            falseButton.isEnabled = true
+            returnButton.isEnabled = false
         }
 
+        checkAnwser.setOnClickListener {
+            showResultDialog()
+        }
         updateQuestion()
-
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "onStart() called")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume() called")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause() called")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop() called")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy() called")
-    }
-
-    private fun checkAnswer(userAnswer: Boolean) {
-        val correctAnswer = quizViewModel.currentQuestionAnswer
-        val messageResId = if (userAnswer == correctAnswer) {
-            countSuccessQuestions++
-
-
-            R.string.correct_toast
-        } else {
-            R.string.incorrect_toast
-        }
-        //Реализация кол-ва правильных ответов
-        if (quizViewModel.currentIndex == quizViewModel.questionBank.size - 1) {
-            Toast.makeText(this, countSuccessQuestions.toString(), Toast.LENGTH_SHORT)
-                .show()
-        }
-        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
-            .show()
-    }
-
-
-    private fun updateQuestion() {
+    fun updateQuestion() {
         val questionTextResId = quizViewModel.currentQuestionText
         questionTextView.setText(questionTextResId)
     }
 
-}
+    fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = quizViewModel.currentQuestionAnswer
+        val messageResId = if (userAnswer == correctAnswer) {
+            countSuccessQuestions++
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+    }
 
+    fun showResultDialog() {
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("Результаты")
+            .setMessage("Количество правильных ответов: $countSuccessQuestions")
+            .setPositiveButton("OK") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .create()
+        dialog.show()
+    }
+}
--- app/src/main/java/com/example/firstapp/viewmodels/QuizViewModel.kt
+++ app/src/main/java/com/example/firstapp/viewmodels/QuizViewModel.kt
@@ -6,6 +6,7 @@ import com.example.firstapp.R
 import com.example.firstapp.models.Question
 
 private const val TAG = "QuizViewModel"
+
 class QuizViewModel : ViewModel() {
     public var currentIndex = 0
     public val questionBank = listOf(
@@ -14,26 +15,22 @@ class QuizViewModel : ViewModel() {
         Question(R.string.question_mideast, false),
         Question(R.string.question_africa, false),
         Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true)
+        Question(R.string.question_asia, true),
     )
 
     val currentQuestionAnswer: Boolean
         get() = questionBank[currentIndex].answer
     val currentQuestionText: Int
         get() = questionBank[currentIndex].textResId
+
     fun moveToNext() {
         currentIndex = (currentIndex + 1) % questionBank.size
-    }
-    fun moveToBack() {
-        if(currentIndex==0){
-            currentIndex = questionBank.size - 1
-        }
-        currentIndex = (currentIndex - 1) % questionBank.size
     }
 
     init {
         Log.d(TAG, "ViewModel instance created")
     }
+
     override fun onCleared() {
         super.onCleared()
         Log.d(TAG, "ViewModel instance about to be destroyed")
